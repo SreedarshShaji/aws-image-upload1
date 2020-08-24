11a1ce4a9b4d0e1762e38b66515aeaba2f667fab
Sreedarsh S <SreedarshShaji@outlook.com>
2020-08-24 23:45:05 +0530

    Boiler plate commit. Basic AWS setup.

---
+++ src/main/java/com/croizzanzlab/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,25 @@
+package com.croizzanzlab.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3()
+    {
+        AWSCredentials awsCredentials=new BasicAWSCredentials("AKIAJJVD6AWUJTUMF7FA",
+                "yDL3/jlADiCaBnv0i9HbSC+wlOUeDjThrcnOxPcg");
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+}
